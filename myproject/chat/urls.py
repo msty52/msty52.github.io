b70b04a791637521835a917f1876240ccd2f2443
msty52 <89525bobbob@gmail.com>
2025-10-24 19:36:54 +0300

    urls.py

--- myproject/chat/urls.py
+++ myproject/chat/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('room/<int:room_id>/', views.room_detail, name='room_detail'),
     path('create-room/', views.create_room, name='create_room'),
     path('room/<int:room_id>/delete/', views.delete_room, name='delete_room'),
+    
+    # Админ URLs
+    path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('admin/ban_user/', views.admin_ban_user, name='admin_ban_user'),
+    path('admin/clear_messages/', views.admin_clear_old_messages, name='admin_clear_messages'),
+    
+    # API URLs
+    path('room/<int:room_id>/send_message/', views.send_message, name='send_message'),
 ]
